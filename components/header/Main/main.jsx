6209058746df28550f697ac184d4b9746d5438f7
AnaCouto1409 <35375044+AnaCouto1409@users.noreply.github.com>
2022-05-22 21:16:00 -0300

    Add files via upload

---
+++ components/header/Main/main.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+
+const [count, setCount]= useState(0) => {
+    return(
+        <div className="App">
+        <header />
+        <main />
+        <footer />
+        </div>
+    )
+}
+export default App;
---
+++ components/header/header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const header = () => {
+  return (
+  <header>
+    <div>header</div>
+    
+    <fieldset>
+        <nav>
+            <ol>
+                <li>Ouvir Rammstein será um diferencial</li>
+                <li>Faça como se deve</li>
+                <li>Tenha soft Skills</li>
+                <li>Do it do it do it do it</li>
+                <li>Vou vencer seremos rápidos como um rio</li>
+                <li><button>Click here!</button></li>
+                <li><button>Click here too</button></li>
+            </ol>
+        </nav>
+    </fieldset>
+    </header>
+  )
+}
+
+export default header
